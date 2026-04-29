c5235f68e1fa65989bc71b0f9ccda90f6fbb1dcc
hakisolos <maxwellexcel174@gmail.com>
2026-04-29 23:47:01 +0100

    initial commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+import { Hono } from "hono";
+
+const app = new Hono()
+
+
+app.get("/", async(c) => {
+    return c.text("app runnning")
+})
+
+
+app.post("/github/webhook", async(c) => {
+    const event = c.req.header('x-github-event')
+    const data = await c.req.json()
+
+    if(event == "push"){
+        console.log("push event detected")
+    }
+    c.status(200)
+})
+
+
+Bun.serve({
+    fetch: app.fetch,
+    port: 4000
+})
+console.log("api is running")
+
